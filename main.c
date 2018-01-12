4380bb2e495b50c14be2843ff81438b327ae3325
wfing <35370971+wfing@users.noreply.github.com>
2018-01-12 21:44:25 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World\n");
-
-	printf("Hello!\n");
-
-    printf("hello git\n");
-	
-	
-
+    printf("Hello World\n");
     printf("xiyouhahahaha");
     printf("Yes\n");
 	return 0;
